958d8658911738f33e19bcf4c0f4eee449c0cea5
VictorRemotes <victor.chen@remotes.com.tw>
2022-01-04 14:33:08 +0800

    220104

---
+++ Log/LevelEnum.cs
@@ -0,0 +1,9 @@
+﻿using NLog;
+
+namespace Log
+{
+    public enum LevelEnum
+    {
+        Debug = 0, Trace, Info, Notice, Warn, Error, Fatal
+    }
+}
---
+++ Log/log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Log
+{
+    internal class Log:Ilog
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public void write(string Content, LevelEnum level)
+        {
+            switch (level)
+            {
+                case LevelEnum.Debug:
+                    logger.Debug($"偵錯(1):{Content}");
+                    break;
+                case LevelEnum.Trace:
+                    logger.Debug($"追蹤(2):{Content}");
+                    break;
+                case LevelEnum.Info:
+                    logger.Debug($"資訊(3):{Content}");
+                    break;
+                case LevelEnum.Notice:
+                    logger.Debug($"通知(4):{Content}");
+                    break;
+                case LevelEnum.Warn:
+                    logger.Debug($"警示(5):{Content}");
+                    break;
+                case LevelEnum.Error:
+                    logger.Debug($"錯誤(6):{Content}");
+                    break;
+                case LevelEnum.Fatal:
+                    logger.Debug($"嚴重(7):{Content}");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
